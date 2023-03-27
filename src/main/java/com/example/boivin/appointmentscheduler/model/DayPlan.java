e044382055a78096172cea7e38c1855d6fb58e0a
Peter Boivin <peterboivin@gmail.com>
2023-03-27 12:33:25 -0400

    Revert "more changes.  This is in progress code.  I don't know if this checkin is any good."
    This reverts commit 6a4a5215fe6683b11e4495e2bebc5dbe9ddc49c6.

--- src/main/java/com/example/boivin/appointmentscheduler/model/DayPlan.java
+++ src/main/java/com/example/boivin/appointmentscheduler/model/DayPlan.java
@@ -1,5 +1,9 @@
 package com.example.boivin.appointmentscheduler.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,11 +25,9 @@ public class DayPlan {
         this.brakes = new ArrayList<>();
     }
 
-    public List<TimePeriod> getTimePeriodsWithBrakesExcluded() {
+    public List<TimePeriod> gettimePeriodsWithBrakesExcluded() {
 
-        if (!timePeriodsWithBrakesExcluded.contains(getWorkingHours())) {
-            timePeriodsWithBrakesExcluded.add(getWorkingHours());
-        }
+        timePeriodsWithBrakesExcluded.add(getWorkingHours());
         List<TimePeriod> brakes = getBrakes();
 
         if (!brakes.isEmpty()) {
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,6 +1,9 @@
-CREATE USER 'peter'@'localhost' IDENTIFIED BY 'peter';
+CREATE USER 'peter'@'172.26.0.3' IDENTIFIED BY 'peter' WITH PASSWORD 'peter';
+
+CREATE USER 'peter'@'%' IDENTIFIED BY 'peter' WITH PASSWORD 'peter';
+
+CREATE DATABASE appointmentscheduler;
+
+GRANT ALL PRIVILEGES ON appointmentscheduler.* to 'peter'@'172.26.0.3';
 
 GRANT ALL PRIVILEGES ON appointmentscheduler.* to 'peter'@'%';
-
-GRANT PROCESS ON *.* TO  'peter'@'%';
-
